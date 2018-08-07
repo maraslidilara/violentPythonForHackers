14597b7e1b31f70219253e4f1b1010c384946b33
maraslidilara <40308789+maraslidilara@users.noreply.github.com>
2018-08-07 10:59:56 -0700

    Update 1-vulnScanner.py

--- CH1/1-vulnScanner.py
+++ CH1/1-vulnScanner.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+# Importing Modules
 import socket
 import os
 import sys
 
-
+'#This function perform action of connecting to the FTP server and returning the banner.
 def retBanner(ip, port):
     try:
         socket.setdefaulttimeout(2)
@@ -15,7 +17,8 @@ def retBanner(ip, port):
     except:
         return
 
-
+'''checkVulns() takes the variable banner as a parameter and then uses it to make 
+a determination of the vulnerability of the server.'''
 def checkVulns(banner, filename):
 
     f = open(filename, 'r')
@@ -42,7 +45,7 @@ def main():
         print '[-] Usage: ' + str(sys.argv[0]) +\
             ' <vuln filename>'
         exit(0)
-
+    
     portList = [21,22,25,80,110,443]
     for x in range(147, 150):
         ip = '192.168.95.' + str(x)
